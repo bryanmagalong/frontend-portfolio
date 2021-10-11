bdd40f0bfe439ff86f3e3916c739a16930c91247
Bryan M <allen.bry95@gmail.com>
2021-10-11 19:51:52 +0200

    feat(theme): add fontSize, padding and gap

--- src/theme/index.js
+++ src/theme/index.js
@@ -10,6 +10,16 @@ const theme = {
     lg: '1024px',
     xl: '1224px',
   },
+  fontSize: {
+    h1: '2.250rem', // 36px
+    h2: '1.625em', // 26px
+  },
+  padding: {
+    section: '6.813rem 1.875rem', // 109px 30px
+  },
+  gap: {
+    landing: '1rem',
+  },
 };
 
 export default theme;
