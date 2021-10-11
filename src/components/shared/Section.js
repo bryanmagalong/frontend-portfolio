d9c7ddeaf537f4c75fe3780b40da9efcb1538f1c
Bryan M <allen.bry95@gmail.com>
2021-10-11 19:52:24 +0200

    feat: add Section and Title shared components

---
+++ src/components/shared/Section.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Title } from './Title';
+
+const Section = ({ gap, landing, title, children }) => {
+  return (
+    <StyledSection landing={landing} gap={gap}>
+      {title && <Title>{title}</Title>}
+      {children}
+    </StyledSection>
+  );
+};
+
+const StyledSection = styled.section`
+  padding: ${(props) =>
+    props.landing ? '2rem 1.875rem' : props.theme.padding.section};
+  min-height: ${(props) =>
+    props.landing ? 'calc(100vh - 3.875rem)' : '100vh'};
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.gap || '1rem'};
+`;
+
+export default Section;
---
+++ src/components/shared/Title.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontSize.h1};
+  font-weight: bold;
+  letter-spacing: .1rem;
+  align-self: center;
+`;
