b0564879a72bb5a453195b36780cb007e0f62968
Bryan M <allen.bry95@gmail.com>
2021-10-25 21:34:09 +0200

    feat: add ContactSpan

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { SocialLinkList } from "../shared/SocialLinkList";
 import { Title } from "../shared/Title";
-import { ContactSection } from "./styled";
+import { ContactSection, ContactSpan } from "./styled";
 
 const Contact = () => {
   return (
     <ContactSection>
       <Title titleColor="#FFF">Contact</Title>
       <SocialLinkList />
+      <ContactSpan>
+        &copy; 2021 - Designed and built by Bryan Magalong
+      </ContactSpan>
     </ContactSection>
   );
 };
--- src/components/Contact/styled.js
+++ src/components/Contact/styled.js
@@ -1,11 +1,27 @@
 import styled from "styled-components";
+import { StyledSocialLinkList } from "../shared/SocialLinkList";
 
 export const ContactSection = styled.section`
   background-color: ${(props) => props.theme.colors.variant};
   min-height: 30vh;
-  padding: 2rem;
+  padding: 2rem 2rem 1rem 2rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+
+  ${StyledSocialLinkList} {
+    flex-grow: 1;
+    align-items: flex-end;
+  }
+`;
+
+export const ContactSpan = styled.span`
+  padding-top: 1rem;
+  color: #fff;
+  font-size: 0.7rem;
+
+  @media (min-width: ${(props) => props.theme.media.sm}) {
+    font-size: 0.8rem;
+  } ;
 `;
